ea9133ce3ee7e85ba71a2a811bd274938a761b2f
Apfirebolt <aspper20@gmail.com>
2025-03-28 03:02:26 +0530

    added search movie

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -25,6 +25,7 @@
 	}
 
 	let movies: Movie[] = [];
+	let searchQuery: string = '';
 	let loading: boolean = true;
 	let error: Error | null = null;
 	let next: string | null = null;
@@ -34,6 +35,24 @@
 		try {
 			loading = true;
 			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			const data: ApiResponse = await response.json();
+			movies = data.results;
+			next = data.next;
+			previous = data.previous;
+		} catch (err) {
+			error = err instanceof Error ? err : new Error('An unknown error occurred');
+		} finally {
+			loading = false;
+		}
+	}
+
+	async function searchMovie(query: string) {
+		try {
+			loading = true;
+			const response = await fetch(`https://softgenie.org/api/movies?search=${query}`);
 			if (!response.ok) {
 				throw new Error(`HTTP error! status: ${response.status}`);
 			}
@@ -99,7 +118,7 @@
 {:else if error}
 	<p class="text-red-500">Error loading movies: {error.message}</p>
 {:else}
-	<div>
+	<div class="bg-accent-dark">
 		<section
 			class="relative h-[500px] bg-cover bg-center"
 			style="background-image: url('https://plus.unsplash.com/premium_photo-1710409625244-e9ed7e98f67b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"
@@ -112,42 +131,44 @@
 						{displayedText}
 					</h1>
 					<p class="mb-6 text-lg md:text-xl">Discover your favorite movies and more</p>
-                    <div class="flex justify-center mx-auto">
-                        <input
-                            type="text"
-                            placeholder="Search for movies..."
-                            class="mr-4 w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                        <button
-                            class="rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white hover:bg-blue-700"
-                        >
-                            Search
-                        </button>
-                    </div>
+					<div class="mx-auto flex justify-center">
+						<input
+							type="text"
+							placeholder="Search for movies..."
+							bind:value={searchQuery}
+							class="text-midnight mr-4 w-full rounded-lg border border-gray-300 px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+						/>
+						<button
+							class="bg-midnight rounded-lg px-6 py-3 font-semibold text-white hover:bg-blue-700"
+							on:click={() => searchMovie(searchQuery)}
+						>
+							Search
+						</button>
+					</div>
 				</div>
 			</div>
 		</section>
-		<div class="container mx-auto">
-			<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-				{#each movies as movie}
-					<div class="rounded-lg bg-accent-light p-4 shadow-md">
-						<h2 class="text-lg font-bold">{movie.Movie_Name}</h2>
-						<p class="text-gray-600">Year: {movie.Year}</p>
-						<p class="text-gray-600">Genre: {movie.Genre}</p>
-						<p class="text-gray-600">Rating: {movie.Rating}</p>
-						<button
-							class="mt-2 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
-							on:click={() => goToMovieDetails(movie)}
-						>
-							<Icon icon="mdi:eye" class="mr-2 inline-block" />
-							View Details
-						</button>
-					</div>
-				{/each}
-			</div>
-			<div class="mt-4 flex justify-center mb-3">
+		<div class="container mx-auto py-2">
+            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                {#each movies as movie}
+                    <div class="bg-accent rounded-lg p-4 shadow-md">
+                        <h2 class="mb-4 bg-white px-2 py-1 text-center text-xl font-bold">{movie.Movie_Name}</h2>
+                        <p class="text-gray-600">Year: {movie.Year}</p>
+                        <p class="text-gray-600">Genre: {movie.Genre}</p>
+                        <p class="text-gray-600">Rating: {movie.Rating}</p>
+                        <button
+                            class="mt-2 rounded bg-bermuda px-4 py-2 text-white hover:bg-green-600"
+                            on:click={() => goToMovieDetails(movie)}
+                        >
+                            <Icon icon="mdi:eye" class="mr-2 inline-block" />
+                            View Details
+                        </button>
+                    </div>
+                {/each}
+            </div>
+            <div class="mt-4 mb-3 flex justify-center">
                 <button
-                    class="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 disabled:opacity-50 flex items-center"
+                    class="bg-midnight hover:bg-midnight flex items-center rounded px-4 py-2 text-white disabled:opacity-50"
                     on:click={goToPreviousPage}
                     disabled={!previous}
                 >
@@ -155,15 +176,15 @@
                     Previous
                 </button>
                 <button
-                    class="rounded bg-blue-500 px-4 py-2 mx-2 text-white hover:bg-blue-600 disabled:opacity-50 flex items-center"
+                    class="bg-midnight hover:bg-midnight mx-2 flex items-center rounded px-4 py-2 text-white disabled:opacity-50"
                     on:click={goToNextPage}
                     disabled={!next}
                 >
                     Next
                     <Icon icon="mdi:arrow-right" class="ml-2" />
                 </button>
-			</div>
-		</div>
+            </div>
+        </div>
 	</div>
 {/if}
 
